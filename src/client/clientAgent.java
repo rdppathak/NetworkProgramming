3ce21f5855ca66b0135430023cef7808d44d0c44
Rushikesh Pathak <rdppathak@gmail.com>
2017-04-28 14:34:12 +0530

    Added basic server and client files

---
+++ src/client/clientAgent.java
@@ -0,0 +1,10 @@
+package client;
+
+public class clientAgent {
+	int port;
+	String ipAddress;
+	
+	public clientAgent(){
+		System.out.println("Initializing clientAgent class");
+	}
+}
---
+++ src/server/serverAgent.java
@@ -0,0 +1,10 @@
+package server;
+
+public class serverAgent {
+	int port;
+	String ipAddress;
+	
+	public serverAgent(){
+		System.out.println("Initializing serverAgent class");
+	}
+}
