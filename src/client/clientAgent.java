844201e2c80f0aa5dd11f6b29a14b5225f4e6257
Rushikesh Pathak <rdppathak@gmail.com>
2017-04-28 14:39:11 +0530

    Getter and setter for port attribute

--- src/client/clientAgent.java
+++ src/client/clientAgent.java
@@ -7,4 +7,12 @@ public class clientAgent {
 	public clientAgent(){
 		System.out.println("Initializing clientAgent class");
 	}
+	
+	public int getPortNumber(){
+		return port;
+	}
+	
+	public void setPortNumber(int port){
+		this.port = port;
+	}
 }
